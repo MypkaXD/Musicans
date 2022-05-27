a146b28f404b961870c44d179991b8fe70613fea
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-05-27 17:07:49 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,51 +2,160 @@
 #include <string>
 #include <vector>
 
+
 class Musicans {
 protected:
-	std::string fullName{""};
+	std::string m_name;
+	std::string m_surname;
+	std::string m_profession;
 public:
-	std::string fullName(std::string fullName) {
-		return fullName;
+	Musicans(std::string name, std::string surname, std::string profession) :
+		m_name(name), m_surname(surname), m_profession(profession) {
 	}
-	void takePlace() {
-		std::cout << "Сел на стул." << std::endl;
+	
+	void print() {
+		std::cout << m_name << " " << m_surname << " is " << m_profession << std::endl;
+	}
+
+	virtual void take_place() {
+		std::cout << "on the base" << std::endl;
+	}
+
+	virtual void rehearsal() {
+		std::cout << "Is up to play" << std::endl;
+	}
+
+	virtual void play() {
+		std::cout << "Is starting to play on" << std::endl;
 	}
 };
 
-class WildInstrumentPlayer :public Musicans {
+class Conductor :public Musicans {
 public:
-	WildInstrumentPlayer(std::string fullName){};
-	void takePlace() {
-		std::cout << getSpec() << " взял дудку" << std::endl;
-		Musicans::takePlace();
+	Conductor(std::string m_name = "", std::string m_surmname = "", std::string m_profession = "") :
+		Musicans(m_name, m_surmname, m_profession) {
 	}
-	std::string getSpec() {
-		std::string spec = "Wild player";
-		return spec;
-	}
-};
 
-class Fleita :public WildInstrumentPlayer {
-public:
-	Fleita(std::string name) :WildInstrumentPlayer(name) {};
 	void play() {
-		std::cout << "Дудит" << std::endl;
+		std::cout << " is starting to conduct" << std::endl;
 	}
-	void repetice() {
-		play();
-	}
-	std::string getSpec() {
-		return "Fleita";
+
+	void rehearsal() {
+		std::cout << "Attention please!" << std::endl;
 	}
 };
 
-class Orchestre {
-	std::vector <Fleita*> fleitist;
+class String :public Musicans {
+public:
+	String(std::string m_name = "", std::string m_surmname = "", std::string m_profession = "") :
+		Musicans(m_name, m_surname, m_profession) {
+	}
+
+	void play_strart() {
+		std::cout << " takes the ";
+	}
+	void play_end() {
+		std::cout << "in his hands and puts his hand on the strings." << std::endl;
+	}
 };
 
-int main()
-{
-	setlocale(LC_ALL, "rus");
-	return 0;
-}
+class Wind :public Musicans {
+public:
+	Wind(std::string m_name = "", std::string m_surmname = "", std::string m_profession = "") :
+		Musicans(m_name, m_surname, m_profession) {
+	}
+	void play_strart() {
+		std::cout << " takes the ";
+	}
+	void play_end() {
+		std::cout << "in his hands and puts it to his mouth." << std::endl;
+	}
+};
+
+class Violinist: public String {
+public:
+	Violinist(std::string m_name = "", std::string m_surmname = "", std::string m_profession = "") :
+		String(m_name, m_surmname, m_profession) {
+	}
+	std::string getProfession() {
+		return m_profession;
+	}
+
+	void rehearsal() {
+		std::cout << getProfession();
+		String::play();
+
+	}
+	void take_place() {
+		std::cout << getProfession();
+		Musicans::take_place();
+
+	}
+	void play() {
+		std::cout << getProfession();
+		String::play_strart();
+		std::cout << "violin ";
+		String::play_end();
+	}
+};
+
+class Flutist :public Wind {
+public:
+	Flutist(std::string m_name = "", std::string m_surmname = "", std::string m_profession = "") :
+		Wind(m_name, m_surmname, m_profession) {
+	}
+	std::string getProfession() {
+		return m_profession;
+	}
+	void take_place() {
+		std::cout << getProfession();
+		Musicans::take_place();
+
+	}
+	void play() {
+		std::cout << getProfession();
+		Wind::play_strart();
+		std::cout << "fluit ";
+		Wind::play_end();
+	}
+};
+
+
+class Orchestr {
+private:
+	std::vector<Musicans*>arr;
+public:
+	Orchestr(std::vector<Musicans*>arr_) {
+		arr.resize(arr_.size());
+		for (int i = 0; i < arr.size(); ++i) {
+			arr[i] = arr_[i];
+		}
+	}
+	Orchestr() {
+	}
+	void take_place(){
+		for (auto v : arr) {
+			v->print();
+			v->take_place();
+		}
+	}
+	void rehearsal() {
+		for (auto v : arr) {
+			v->print();
+			v->rehearsal();
+		}
+
+	}
+	void play(){
+		for (auto v : arr) {
+			v->print();
+			v->play();
+		}
+
+	}
+	void add_mus(Musicans& t){
+		arr.push_back(&t);
+	}
+	void count_flutist() {
+	}
+};
